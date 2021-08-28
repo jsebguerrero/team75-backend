908760ed0ed244671d642e28c9c0622624e5c5ef
Juan S Guerrero C <36944059+jsebguerrero@users.noreply.github.com>
2021-08-28 17:00:41 -0500

    Update __init__.py

--- settings_handler/__init__.py
+++ settings_handler/__init__.py
@@ -2,10 +2,10 @@ from dotenv import load_dotenv
 from dotenv_settings_handler import BaseSettingsHandler
 import os
 __all__ = ("settings",)
-
+#load .env file injected via docker or the existing one
 load_dotenv()
 
-
+#settings
 class Settings(BaseSettingsHandler):
     port = os.getenv('PORT')
     host = os.getenv('HOST')
