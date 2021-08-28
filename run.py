965756c7f2d0a9fd60c19b2ade85b18562589e39
Juan S Guerrero C <36944059+jsebguerrero@users.noreply.github.com>
2021-08-28 16:53:58 -0500

    Update run.py

--- run.py
+++ run.py
@@ -2,10 +2,11 @@ import uvicorn
 from settings_handler import settings
 
 import os
-
+#select direct path to the app
 PATH = os.path.dirname(os.path.realpath(__file__))
 
 if __name__ == "__main__":
+    #invoke uvicorn to set up the ASGI for the app, this includes the port, host and # workers
     uvicorn.run(
         "app:app",
         host = settings.host,
